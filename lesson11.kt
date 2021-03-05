bb4b37b99fedab068a0305eaeff83dfdefe256af
agamonch <jnkyrgyz@gmail.com>
2021-03-05 18:43:57 +0300

    added lesson11 file

---
+++ lesson11.kt
@@ -0,0 +1,7 @@
+var myMap: HashMap<String, String>? = null
+fun main() {
+    val names = arrayOf("KG", "RU", "USA", "TR")
+    val codes = arrayOf("+996", "+7", "+1", "+98")
+    myMap = hashMapOf()
+
+}

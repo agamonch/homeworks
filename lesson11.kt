9072376f6afb2d79b729928864e9b12d77211ea9
agamonch <jnkyrgyz@gmail.com>
2021-03-06 00:26:42 +0300

    added

--- lesson11.kt
+++ lesson11.kt
@@ -2,6 +2,8 @@ var myMap: HashMap<String, String>? = null
 fun main() {
     val names = arrayOf("KG", "RU", "USA", "TR")
     val codes = arrayOf("+996", "+7", "+1", "+98")
-    myMap = hashMapOf()
 
+    var myMap = mutableMapOf<String, String>()
+            .apply { for (name in names.indices) this[names[name]] = codes[name] }
+        println("Codes of countries - $myMap")
 }

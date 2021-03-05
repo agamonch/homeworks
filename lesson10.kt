4ec378e17be6e0324ad2e5d01370e849b46e69b2
agamonch <jnkyrgyz@gmail.com>
2021-03-05 18:30:28 +0300

    changed lesson10. Added 2 items in shoppingList

--- lesson10.kt
+++ lesson10.kt
@@ -1,6 +1,10 @@
 fun main() {
     var itemCosts = mapOf("Bread" to 50)
     var shoppingList = listOf("Bread")
+    itemCosts+=("Milk" to 20)
+    itemCosts+=("Meat" to 150)
+    shoppingList+= ("Milk")
+    shoppingList+= ("Meat")
     var totalCosts = 0
     for (item in shoppingList) {
         val itemCos = itemCosts[item]

32a6093938a782ceb272715aecae64cd74a0773c
agamonch <jnkyrgyz@gmail.com>
2021-03-21 00:27:47 +0300

    added some classes and property's

--- lesson18.kt
+++ lesson18.kt
@@ -1,1 +1,60 @@
-git
+import java.util.*
+
+fun main() {
+
+}
+
+class Cat{
+    val name = "Tom"
+    val breed = "Bengal"
+    val age: Int = 3
+
+}
+class Food {
+    val name = "Besh barmak"
+    val ingred = "meat and dough"
+    val price = 200
+
+}
+class Paper {
+    val color = "white"
+    val format = "A4"
+    val unit = 100
+}
+class Boiler{
+    val capacity = "80l"
+    val model = "Ariston"
+    val price = 20000
+}
+class Battery{
+    val name = "varta"
+    val capac = "1.5 v"
+    val form = "cylinder"
+}
+class Liquid {
+    val name = "water"
+    val formula = "H2O"
+    val kyrgyz = "Suu"
+}
+class Cup{
+    val material = "glass"
+    val capacity = "200 g"
+    val unit = 6
+}
+class Box {
+    val form = "parallelepiped"
+    val material = "paper"
+    val capacity = "50 l"
+
+}
+class Student{
+    val name = "Asan"
+    val age = 21
+    val status = "Hungry"
+}
+class CompanyMember {
+    val name = "Elon"
+    val company = "spasex"
+    val position = "Leader"
+    val status = "Genius"
+}

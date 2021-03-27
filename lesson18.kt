7db7248969687b935a5a7bd8df2ce2d2b45fcd1a
agamonch <jnkyrgyz@gmail.com>
2021-03-27 21:35:31 +0300

    classes changed to date classes

--- lesson18.kt
+++ lesson18.kt
@@ -2,67 +2,72 @@ import java.util.*
 
 fun main() {
     val cat = Cat("Tom", "Bengal", 3, 5, 6)
-    println("Cat's name is ${cat.name}, age ${cat.age}, satiety level = ${cat.satietyLevel}, water level = ${cat.waterLevel} ")
+    val food = Food("Besh barmak", "meat and dough", 200)
+    val paper = Paper("white", "A4", 100)
+    val boiler = Boiler("80l", "Ariston", 20000)
+    val battery = Battery("Varta", "1.5v", "cylinder")
+    val liquid = Liquid("Water", "H2O", "Suu")
+    val cup = Cup("Glass", "200g", 6)
+    val box = Box("parallelepiped", "paper", "50l")
+    val student = Student("Asan", 21, "hungry")
+    val companymember = CompanyMember("Elon", "spasex", "leader", "genius" )
 
 }
 
-class Cat(name: String, breed: String, age: Int, satietyLevel: Int, waterLevel: Int){
-    val name = "Tom"
-    val breed = "Bengal"
-    val age = 3
-    var satietyLevel = 2
-    var waterLevel = 3
-    init {
-        this.satietyLevel = satietyLevel
-        this.waterLevel = waterLevel
-    }
+data class Cat(
+    val name: String,
+    val breed: String,
+    val age: Int,
+    var satietyLevel: Int,
+    var waterLevel: Int)
 
-}
-class Food {
-    val name = "Besh barmak"
-    val ingred = "meat and dough"
-    val price = 200
 
-}
-class Paper {
-    val color = "white"
-    val format = "A4"
-    val unit = 100
-}
-class Boiler{
-    val capacity = "80l"
-    val model = "Ariston"
-    val price = 20000
-}
-class Battery{
-    val name = "varta"
-    val capac = "1.5 v"
-    val form = "cylinder"
-}
-class Liquid {
-    val name = "water"
-    val formula = "H2O"
-    val kyrgyz = "Suu"
-}
-class Cup{
-    val material = "glass"
-    val capacity = "200 g"
-    val unit = 6
-}
-class Box {
-    val form = "parallelepiped"
-    val material = "paper"
-    val capacity = "50 l"
 
-}
-class Student{
-    val name = "Asan"
-    val age = 21
-    val status = "Hungry"
-}
-class CompanyMember {
-    val name = "Elon"
-    val company = "spasex"
-    val position = "Leader"
-    val status = "Genius"
-}
+
+data class Food(
+    val name: String,
+    val ingred: String,
+    val price: Int)
+
+data class Paper (
+    val color: String,
+    val format: String,
+    val unit: Int
+)
+data class Boiler(
+    val capacity: String,
+    val model: String,
+    val price: Int,
+)
+data class Battery(
+    val name: String,
+    val capac: String,
+    val form: String,
+)
+data class Liquid (
+    val name: String,
+    val formula: String,
+    val kyrgyz: String,
+)
+data class Cup(
+    val material: String,
+    val capacity: String,
+    val unit: Int
+)
+data class Box (
+    val form: String,
+    val material: String,
+    val capacity: String,
+
+)
+data class Student(
+    val name: String,
+    val age: Int,
+    val status: String,
+)
+data class CompanyMember (
+    val name : String,
+    val company: String,
+    val position : String,
+    val status : String,
+)
